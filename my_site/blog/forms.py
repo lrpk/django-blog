94b8e7e8c231d1b09a0aa119fa90429816b3c76e
Pavlo Klymenko <lorrdpk@gmail.com>
2023-08-28 12:04:07 +0300

    first blog commit

---
+++ my_site/blog/forms.py
@@ -0,0 +1,13 @@
+from django.forms import ModelForm
+
+from .models import Comment
+
+class CommentForm(ModelForm):
+    class Meta:
+        model = Comment
+        exclude = ["post"]
+        labels = {
+            "user_name": "Your Name",
+            "user_email": "Your Email",
+            "text": "Your Comment"
+        }
